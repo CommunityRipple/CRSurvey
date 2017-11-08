dbbc3a9a3cec20a4ac0d9603c0d579a69b0e5eea
Rasheed Abdul-Aziz <squeedee@gmail.com>
2017-11-07 21:29:58 -0500

    Move update-survey to npm scripts

--- scripts/update-survey.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
-
-echo "Downloading survey. Edit it here: https://surveyjs.io/Service/EditSurvey/7da3d70b-c4d2-4409-85a1-27353475c64d"
-echo
-echo
-curl https://dxsurveyapi.azurewebsites.net/api/Survey/getSurvey?surveyId=7da3d70b-c4d2-4409-85a1-27353475c64d -o ${ROOT}/src/survey.json
