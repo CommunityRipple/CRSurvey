5be5153054c97f190a763413e8c8b965804cfc94
Rasheed Abdul-Aziz <squeedee@gmail.com>
2017-11-07 21:24:59 -0500

    improve look of background

--- src/Layout.js
+++ src/Layout.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import {Grid as Container, PageHeader} from 'react-bootstrap';
+import {Grid as Container, Image} from 'react-bootstrap';
 
 export default function Layout({children}) {
+    const ground = require('../img/ground.png');
+    const left = require('../img/cloud-left.png');
+    const right = require('../img/cloud-right.png');
+
     return (
-        <Container fluid={true}>
-            {children}
-        </Container>
+        <div>
+            <div className="bg-gradient" />
+            <Image className="bg-image bg-cloud-right" src={right}/>
+            <Image className="bg-image bg-cloud-left" src={left}/>
+            <Image className="bg-image bg-ground" src={ground}/>
+            <Container fluid={true}>
+                {children}
+            </Container>
+        </div>
     );
 }
